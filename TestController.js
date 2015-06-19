1f864744fa8e34a91a370a91d2b0217d46bc207f
Steve Kane <kanesteven@gmail.com>
2015-06-19 10:35:29 -0500

    added counters for both applications to show signs of functionality

--- TestController.js
+++ TestController.js
@@ -1,4 +1,9 @@
 angular.module('plugin')
-.controller('TestCtrl', [function () {
+.controller('TestCtrl', ['$interval', function ($interval) {
+  var self = this
+
   this.evidence = "The plugin works"
+  this.counter = 0
+
+  $interval(function () { self.counter++ }, 500)
 }])
--- index.js
+++ index.js
@@ -5,7 +5,7 @@
   var hostDiv = document.createElement('div')
 
   hostDiv.setAttribute('ng-controller', 'TestCtrl as ctrl')
-  hostDiv.innerText = '{{ctrl.evidence}}'
+  hostDiv.innerText = '{{ctrl.evidence}}\n{{ctrl.counter}}'
   document.body.appendChild(hostDiv)
   angular.module('plugin', [])
   require('./TestController')
